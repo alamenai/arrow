4a3d2e0e9663f074ac81d1ec9d1dbdb7ab8632f8
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-02 07:39:05 -0700

    :cyclone: Replace default export by export for each function

--- src/1.x.x/scripts/array/has/boolean.js
+++ src/1.x.x/scripts/array/has/boolean.js
@@ -2,15 +2,15 @@ import {
     checkArray,
 } from "../../../helpers/check";
 
-function hasAnyBoolean(array) {
+export function hasAnyBoolean(array) {
     return checkArray(array) && (!array.includes(true) && !array.includes(false));
 }
 
-function hasAtLeastBoolean(array) {
+export function hasAtLeastBoolean(array) {
     return checkArray(array) && (array.includes(true) || array.includes(false));
 }
 
-function hasOnceBoolean(array) {
+export function hasOnceBoolean(array) {
     return checkArray(array) && (array.reduce((acc, element) => {
         if (acc === 2) return 2;
         if (typeof element === "boolean") acc += 1;
@@ -18,30 +18,21 @@ function hasOnceBoolean(array) {
     }, 0) === 1);
 }
 
-function hasOnlyBoolean(array) {
+export function hasOnlyBoolean(array) {
     return checkArray(array) && array.every((element) => typeof element === "boolean");
 }
 
-function hasPairBoolean(array) {
+export function hasPairBoolean(array) {
     return checkArray(array) && (array.reduce((acc, element) => {
         if (typeof element === "boolean") acc += 1;
         return acc;
     }, 0) === 2);
 }
 
-function hasExactBoolean(array, nullCount) {
+export function hasExactBoolean(array, nullCount) {
     if (typeof nullCount !== "number") throw new Error("The second parameter should be a number");
     return checkArray(array) && (array.reduce((acc, element) => {
         if (typeof element === "boolean") acc += 1;
         return acc;
     }, 0) === nullCount);
 }
-
-export {
-    hasAnyBoolean,
-    hasAtLeastBoolean,
-    hasOnceBoolean,
-    hasOnlyBoolean,
-    hasPairBoolean,
-    hasExactBoolean,
-};
