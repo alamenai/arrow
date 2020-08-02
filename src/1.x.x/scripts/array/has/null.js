599344af56f78a254a2ead2662f92bc0bdce5c49
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-02 13:22:09 -0700

    :cyclone: Extract Null checking in each function into isNull()

--- src/1.x.x/scripts/array/has/null.js
+++ src/1.x.x/scripts/array/has/null.js
@@ -2,29 +2,33 @@ import {
     checkArray,
 } from "../../../helpers/check";
 
+function isNull(element) {
+    return element === null
+}
+
 export function hasAnyNull(array) {
-    return checkArray(array) && !array.every(element => element === null);
+    return checkArray(array) && !array.every(element => isNull(element));
 }
 
 export function hasAtLeastNull(array) {
-    return checkArray(array) && array.some(element => element === null);
+    return checkArray(array) && array.some(element => isNull(element));
 }
 
 export function hasOnceNull(array) {
     return checkArray(array) && (array.reduce((acc, element) => {
         if (acc === 2) return 2;
-        if (element === null) acc += 1;
+        if (isNull(element)) acc += 1;
         return acc;
     }, 0) === 1);
 }
 
 export function hasOnlyNull(array) {
-    return checkArray(array) && array.every((element) => element === null);
+    return checkArray(array) && array.every((element) => isNull(element));
 }
 
 export function hasPairNull(array) {
     return checkArray(array) && (array.reduce((acc, element) => {
-        if (element === null) acc += 1;
+        if (isNull(element)) acc += 1;
         return acc;
     }, 0) === 2);
 }
@@ -32,7 +36,7 @@ export function hasPairNull(array) {
 export function hasExactNull(array, nullCount) {
     if (typeof nullCount !== "number") throw new Error("The second parameter should be a number");
     return checkArray(array) && (array.reduce((acc, element) => {
-        if (element === null) acc += 1;
+        if (isNull(element)) acc += 1;
         return acc;
     }, 0) === nullCount);
 }
