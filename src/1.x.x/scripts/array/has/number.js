bf6ecb6f9fb94b654b771ab3eafc0ee54e13fa0a
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-02 05:11:35 -0700

    :cyclone: Rename value parameter to element

--- src/1.x.x/scripts/array/has/number.js
+++ src/1.x.x/scripts/array/has/number.js
@@ -1,40 +1,47 @@
-import { checkArray } from "../../../helpers/check";
+import {
+    checkArray
+} from "../../../helpers/check";
 
 function hasAnyNumber(array) {
-  return checkArray(array) && array.every((value) => typeof value !== "number");
+    return checkArray(array) && array.every((element) => typeof value !== "number");
 }
 
 function hasAtLeastNumber(array) {
-  return checkArray(array) && array.some((value) => typeof value === "number");
+    return checkArray(array) && array.some((element) => typeof value === "number");
 }
 
 function hasOnceNumber(array) {
-  return checkArray(array) && (array.reduce((acc, value) => {
-    if (acc === 2) return 2;
-    if (typeof value === "number") acc += 1;
-    return acc;
-  }, 0) === 1);
+    return checkArray(array) && (array.reduce((acc, element) => {
+        if (acc === 2) return 2;
+        if (typeof element === "number") acc += 1;
+        return acc;
+    }, 0) === 1);
 }
 
 function hasOnlyNumber(array) {
-  return checkArray(array) && array.every((value) => typeof value === "number");
+    return checkArray(array) && array.every((element) => typeof element === "number");
 }
 
 function hasPairNumber(array) {
-  return checkArray(array) && (array.reduce((acc, value) => {
-    if (typeof value === "number") acc += 1;
-    return acc;
-  }, 0) === 2);
+    return checkArray(array) && (array.reduce((acc, element) => {
+        if (typeof element === "number") acc += 1;
+        return acc;
+    }, 0) === 2);
 }
 
 function hasExactNumber(array, numberCount) {
-  if (typeof numberCount !== "number") throw new Error("The second parameter should be a number");
-  return checkArray(array) && (array.reduce((acc, value) => {
-    if (typeof value === "number") acc += 1;
-    return acc;
-  }, 0) === numberCount);
+    if (typeof numberCount !== "number") throw new Error("The second parameter should be a number");
+    return checkArray(array) && (array.reduce((acc, element) => {
+        if (typeof element === "number") acc += 1;
+        return acc;
+    }, 0) === numberCount);
 }
 
 export default {
-  hasAnyNumber, hasAtLeastNumber, hasOnceNumber, hasOnlyNumber, hasPairNumber, hasExactNumber,
+    hasAnyNumber,
+    hasAtLeastNumber,
+    hasOnceNumber,
+    hasOnlyNumber,
+    hasPairNumber,
+    hasExactNumber,
 };
