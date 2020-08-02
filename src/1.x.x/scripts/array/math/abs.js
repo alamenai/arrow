01522753c7a348f444402f80b92155a9248e7e69
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-02 15:27:07 -0700

    :tada: Create abs.js and implement abs function

---
+++ src/1.x.x/scripts/array/math/abs.js
@@ -0,0 +1,16 @@
+import {
+    checkArray
+} from "../../../helpers/check";
+
+export function abs(array, from, to) {
+    if (checkArray(array)) {
+        return array.map(element => (array.indexOf(element) >= from && array.indexOf(element) < to + 1) && Math.abs(element))
+    }
+}
+
+// export function absAll(array = [], items) {
+//     // Concat the value with all items
+//     // Suppose the value is 'deno'
+//     // [1, 2, 3] will be [1deno, 2deno, 3deno]
+//     // Warning: If the value is a number, make sure the operation is concatenation not an addition
+// }
