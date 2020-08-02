7fd952db94ce793d42dc9fbe220417bf57b6f6ca
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-02 05:07:59 -0700

    :cyclone: Rename value parameter to element

--- src/1.x.x/scripts/array/has/boolean.js
+++ src/1.x.x/scripts/array/has/boolean.js
@@ -11,28 +11,28 @@ function hasAtLeastBoolean(array) {
 }
 
 function hasOnceBoolean(array) {
-    return checkArray(array) && (array.reduce((acc, value) => {
+    return checkArray(array) && (array.reduce((acc, element) => {
         if (acc === 2) return 2;
-        if (typeof value === 'boolean') acc += 1;
+        if (typeof element === 'boolean') acc += 1;
         return acc;
     }, 0) === 1);
 }
 
 function hasOnlyBoolean(array) {
-    return checkArray(array) && array.every((value) => typeof value === 'boolean');
+    return checkArray(array) && array.every((element) => typeof element === 'boolean');
 }
 
 function hasPairBoolean(array) {
-    return checkArray(array) && (array.reduce((acc, value) => {
-        if (typeof value === 'boolean') acc += 1;
+    return checkArray(array) && (array.reduce((acc, element) => {
+        if (typeof element === 'boolean') acc += 1;
         return acc;
     }, 0) === 2);
 }
 
 function hasExactBoolean(array, nullCount) {
     if (typeof nullCount !== "number") throw new Error("The second parameter should be a number");
-    return checkArray(array) && (array.reduce((acc, value) => {
-        if (typeof value === 'boolean') acc += 1;
+    return checkArray(array) && (array.reduce((acc, element) => {
+        if (typeof element === 'boolean') acc += 1;
         return acc;
     }, 0) === nullCount);
 }
