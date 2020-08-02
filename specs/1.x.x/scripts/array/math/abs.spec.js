ded7ec357a050271b9751cff7e7f986c7fef7b6c
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-02 16:42:21 -0700

    :tada: Create and implement abs.spec.js for Testing abs.js

---
+++ specs/1.x.x/scripts/array/math/abs.spec.js
@@ -0,0 +1,53 @@
+import {
+    abs
+} from "../../../../../src/1.x.x/scripts/array/math/abs"
+
+describe("Absolute values for specific range of elements", () => {
+    it("should return positive values for positive values", () => {
+        expect(abs([1, 2, 3], 0, 1)).toStrictEqual([1, 2, 3]);
+    });
+    it("should return positive values for the negative values", () => {
+        expect(abs([-1, -2, -3], 0, 1)).toStrictEqual([1, 2, -3]);
+    });
+    it("should return positive values for the negative values and skip values with type is not equal to number", () => {
+        expect(abs([-1, -2, -3, "a", "b"], 0, 1)).toStrictEqual([1, 2, -3, "a", "b"]);
+    });
+    it("should return the same values that are not numbers", () => {
+        expect(abs(["a", "b", "c"], 0, 1)).toStrictEqual(["a", "b", "c"]);
+    });
+
+
+    it("should throw an error", () => {
+
+        function passNumber() {
+            abs(1, 0, 1)
+        }
+
+        function passString() {
+            abs("array", 0, 1)
+        }
+
+        function passEmptyArray() {
+            abs([], 0, 1)
+        }
+
+        function passNull() {
+            abs(null, 0, 1)
+        }
+
+        function passUndefined() {
+            abs(undefined, 0, 1)
+        }
+
+        function passBigIndex() {
+            const array = [-1, -2, -3];
+            abs(array, 0, 4); // The last index of array is 2
+        }
+        expect(passNumber).toThrowError("The parameter should be an array");
+        expect(passString).toThrowError("The parameter should be an array");
+        expect(passEmptyArray).toThrowError("The array is empty");
+        expect(passNull).toThrowError("The parameter is null or undefined");
+        expect(passUndefined).toThrowError("The parameter is null or undefined");
+        expect(passBigIndex).toThrowError("The last index of the array is less then 'to' parameter");
+    });
+});
--- src/1.x.x/scripts/array/math/abs.js
+++ src/1.x.x/scripts/array/math/abs.js
@@ -5,7 +5,7 @@ import {
 export function abs(array, from, to) {
     if (checkArray(array)) {
         if (to < array.length) {
-            return array.map(element => ((array.indexOf(element) >= from && array.indexOf(element) < to + 1) && Math.abs(element)) || element)
+            return array.map(element => ((array.indexOf(element) >= from && array.indexOf(element) < to + 1) && typeof element === "number" && Math.abs(element)) || element)
         } else {
             throw new Error("The last index of the array is less then 'to' parameter")
         }
