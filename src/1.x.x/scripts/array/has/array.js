e4df361c7fccb3183e538c8d8a6a6c58e6e53db1
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-02 07:40:34 -0700

    :cyclone: Replace default export by export for each function

--- src/1.x.x/scripts/array/has/array.js
+++ src/1.x.x/scripts/array/has/array.js
@@ -2,15 +2,15 @@ import {
     checkArray,
 } from "../../../helpers/check";
 
-function hasAnyArray(array) {
+export function hasAnyArray(array) {
     return checkArray(array) && !array.every((element) => Array.isArray(element));
 }
 
-function hasAtLeastArray(array) {
+export function hasAtLeastArray(array) {
     return checkArray(array) && array.some((element) => Array.isArray(element));
 }
 
-function hasOnceArray(array) {
+export function hasOnceArray(array) {
     return checkArray(array) && (array.reduce((acc, element) => {
         if (acc === 2) return 2;
         if (Array.isArray(element)) acc += 1;
@@ -18,30 +18,21 @@ function hasOnceArray(array) {
     }, 0) === 1);
 }
 
-function hasOnlyArray(array) {
+export function hasOnlyArray(array) {
     return checkArray(array) && array.every((element) => Array.isArray(element));
 }
 
-function hasPairArray(array) {
+export function hasPairArray(array) {
     return checkArray(array) && (array.reduce((acc, element) => {
         if (Array.isArray(element)) acc += 1;
         return acc;
     }, 0) === 2);
 }
 
-function hasExactArray(array, nullCount) {
+export function hasExactArray(array, nullCount) {
     if (typeof nullCount !== "number") throw new Error("The second parameter should be a number");
     return checkArray(array) && (array.reduce((acc, element) => {
         if (Array.isArray(element)) acc += 1;
         return acc;
     }, 0) === nullCount);
 }
-
-export {
-    hasAnyArray,
-    hasAtLeastArray,
-    hasOnceArray,
-    hasOnlyArray,
-    hasPairArray,
-    hasExactArray,
-};
