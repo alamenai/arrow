c9fbde9e428da49c4e6b713c98f8fe23685ed9b0
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-03 05:19:29 -0700

    :wastebasket: Remove isBooleanArray() and import it from external module

--- src/1.x.x/scripts/array/math/acc.js
+++ src/1.x.x/scripts/array/math/acc.js
@@ -2,6 +2,10 @@ import {
     checkArray
 } from "../../../helpers/check";
 
+import {
+    isBooleanArray
+} from "../../../helpers/is";
+
 export function accBoolean(array) {
     if (checkArray(array)) {
         return isBooleanArray(array) ? array.reduce((previous, next) => previous && next) : array
@@ -12,8 +16,4 @@ export function accAll(array) {
     if (checkArray(array)) {
         return array.reduce((previous, next) => previous + next)
     }
-}
-
-function isBooleanArray(array) {
-    return array.every(element => typeof element === "boolean")
 }
