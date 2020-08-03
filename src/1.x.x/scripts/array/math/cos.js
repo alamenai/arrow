accbfa8c49805d05aac4becc6fd6e79b4723461b
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-03 05:35:35 -0700

    Revert ":tada: Create and implement is.spec.js for Testing is.js"
    This reverts commit aaface1907e329d293ad1df909afd65f59f5f50f.

--- src/1.x.x/scripts/array/math/cos.js
+++ src/1.x.x/scripts/array/math/cos.js
@@ -5,7 +5,7 @@ import {
 export function cos(array, from, to) {
     if (checkArray(array)) {
         if (to < array.length) {
-            return array.map(element => (typeof element === "number" && Math.cos(element)) || element)
+            return array.map(element => ((array.indexOf(element) >= from && array.indexOf(element) < to + 1) && typeof element === "number" && Math.cos(element)) || element)
         } else {
             throw new Error("The last index of the array is less then 'to' parameter")
         }
