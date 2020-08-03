a6151ea131a0a912bb9248878cbd2346a550184f
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-03 09:46:00 -0700

    Revert ":cyclone: Rename cos() to cosRange() and cosAll() to cos()"
    This reverts commit ef16f4150075e710b856a83f2fe468aca6fdba92.

--- specs/1.x.x/scripts/array/math/cos.spec.js
+++ specs/1.x.x/scripts/array/math/cos.spec.js
@@ -1,21 +1,21 @@
 import {
-    cosRange,
-    cos
+    cos,
+    cosAll
 } from "../../../../../src/1.x.x/scripts/array/math/cos"
 
 describe("Cosinus values for specific range of elements", () => {
     it("should return Cosinus for only the two first elements", () => {
-        expect(cosRange([Math.PI, 0, 3], 0, 1)).toStrictEqual([-1, 1, 3]);
+        expect(cos([Math.PI, 0, 3], 0, 1)).toStrictEqual([-1, 1, 3]);
     });
 
     it("should return the same values that are not numbers", () => {
-        expect(cosRange(["a", "b", "c"], 0, 1)).toStrictEqual(["a", "b", "c"]);
+        expect(cos(["a", "b", "c"], 0, 1)).toStrictEqual(["a", "b", "c"]);
     });
 
     it("should throw an error", () => {
         function passBigIndex() {
             const array = [-1, -2, -3];
-            cosRange(array, 0, 4); // The last index of array is 2
+            cos(array, 0, 4); // The last index of array is 2
         }
         expect(passBigIndex).toThrowError("The last index of the array is less then 'to' parameter");
     });
@@ -23,10 +23,10 @@ describe("Cosinus values for specific range of elements", () => {
 
 describe("Cosinus for all elements", () => {
     it("should return Cosinus of all elements", () => {
-        expect(cos([Math.PI, Math.PI, 0])).toStrictEqual([-1, -1, 1]);
-        expect(cos([0, 0, 0])).toStrictEqual([1, 1, 1]);
+        expect(cosAll([Math.PI, Math.PI, 0])).toStrictEqual([-1, -1, 1]);
+        expect(cosAll([0, 0, 0])).toStrictEqual([1, 1, 1]);
     });
     it("should return the same values that are not numbers", () => {
-        expect(cos(["a", "b", "c"])).toStrictEqual(["a", "b", "c"]);
+        expect(cosAll(["a", "b", "c"])).toStrictEqual(["a", "b", "c"]);
     });
 });
--- src/1.x.x/scripts/array/math/cos.js
+++ src/1.x.x/scripts/array/math/cos.js
@@ -5,7 +5,7 @@ import {
 export function cosRange(array, from, to) {
     if (checkArray(array)) {
         if (to < array.length) {
-            return array.map(element => ((array.indexOf(element) >= from && array.indexOf(element) < to + 1) && typeof element === "number" && (element = Math.cos(element))) || element)
+            return array.map(element => ((array.indexOf(element) >= from && array.indexOf(element) < to + 1) && typeof element === "number" && Math.cos(element)) || element)
         } else {
             throw new Error("The last index of the array is less then 'to' parameter")
         }
@@ -14,6 +14,6 @@ export function cosRange(array, from, to) {
 
 export function cos(array) {
     if (checkArray(array)) {
-        return array.map(element => (typeof element === "number" && (element = Math.cos(element))) || element)
+        return array.map(element => (typeof element === "number" && Math.cos(element)) || element)
     }
 }
