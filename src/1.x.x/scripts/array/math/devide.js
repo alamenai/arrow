fd7f9e4231fe29dec67aa7d1427f1a580751aae9
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-03 15:22:40 -0700

    :cyclone: Rename devide() to devideRange() and devideAll() to devide()

--- src/1.x.x/scripts/array/math/devide.js
+++ src/1.x.x/scripts/array/math/devide.js
@@ -2,7 +2,7 @@ import {
     checkArray
 } from "../../../helpers/check";
 
-export function devide(array, from, to, devider) {
+export function devideRange(array, from, to, devider) {
     if (checkArray(array)) {
         if (to < array.length) {
             return array.map(element => ((array.indexOf(element) >= from && array.indexOf(element) < to + 1) && typeof element === "number" && (element = element / devider)) || element)
@@ -18,7 +18,7 @@ export function devideSelf(array) {
     }
 }
 
-export function devideAll(array, devider) {
+export function devide(array, devider) {
     if (checkArray(array)) {
         return array.map(element => (typeof element === "number" && (element = element / devider)) || element)
     }
