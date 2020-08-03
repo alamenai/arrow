15052d4f08ea9481eb3186200936152eb32a8e8e
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-03 10:15:55 -0700

    :tada: Create and implement zero.js

---
+++ src/1.x.x/scripts/array/math/zero.js
@@ -0,0 +1,19 @@
+import {
+    checkArray
+} from "../../../helpers/check";
+
+export function zeroRange(array, from, to) {
+    if (checkArray(array)) {
+        if (to < array.length) {
+            return array.map(element => ((array.indexOf(element) >= from && array.indexOf(element) < to + 1) && typeof element === "number" && (element = 0)) || element)
+        } else {
+            throw new Error("The last index of the array is less then 'to' parameter")
+        }
+    }
+}
+
+export function zero(array) {
+    if (checkArray(array)) {
+        return array.map(element => (typeof element === "number" && (element = 0)) || element)
+    }
+}
