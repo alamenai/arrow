46550640f39a72bfae8429ed05211e80c6502a1e
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-02 17:37:51 -0700

    :tada: Create and implement acc.spec.js for Testing acc.js

---
+++ specs/1.x.x/scripts/array/math/acc.spec.js
@@ -0,0 +1,90 @@
+import {
+    accAll,
+    accBoolean
+} from "../../../../../src/1.x.x/scripts/array/math/acc"
+
+describe("Accumulate all elements", () => {
+    it("should return +6", () => {
+        expect(accAll([1, 2, 3])).toStrictEqual(6);
+    });
+    it("should return -6", () => {
+        expect(accAll([-1, -2, -3])).toStrictEqual(-6);
+    });
+    it("should concat all strings in one string", () => {
+        expect(accAll(["a", "b", "c"])).toStrictEqual("abc");
+    });
+    it("should return the result of && between boolean elements", () => {
+        expect(accAll([true, false, true])).toStrictEqual(2);
+    });
+
+    it("should throw an error", () => {
+
+        function passNumber() {
+            accAll(1)
+        }
+
+        function passString() {
+            accAll("array")
+        }
+
+        function passEmptyArray() {
+            accAll([])
+        }
+
+        function passNull() {
+            accAll(null)
+        }
+
+        function passUndefined() {
+            accAll(undefined)
+        }
+
+        expect(passNumber).toThrowError("The parameter should be an array");
+        expect(passString).toThrowError("The parameter should be an array");
+        expect(passEmptyArray).toThrowError("The array is empty");
+        expect(passNull).toThrowError("The parameter is null or undefined");
+        expect(passUndefined).toThrowError("The parameter is null or undefined");
+    });
+});
+
+
+describe("Accumulate boolean elements", () => {
+    it("should return true", () => {
+        expect(accBoolean([true, true, true])).toStrictEqual(true);
+    });
+    it("should return false", () => {
+        expect(accBoolean([false, false, false])).toStrictEqual(false);
+    });
+    it("should return false", () => {
+        expect(accBoolean([false, true, false])).toStrictEqual(false);
+    });
+
+    it("should throw an error", () => {
+
+        function passNumber() {
+            accBoolean(1)
+        }
+
+        function passString() {
+            accBoolean("array")
+        }
+
+        function passEmptyArray() {
+            accBoolean([])
+        }
+
+        function passNull() {
+            accBoolean(null)
+        }
+
+        function passUndefined() {
+            accBoolean(undefined)
+        }
+
+        expect(passNumber).toThrowError("The parameter should be an array");
+        expect(passString).toThrowError("The parameter should be an array");
+        expect(passEmptyArray).toThrowError("The array is empty");
+        expect(passNull).toThrowError("The parameter is null or undefined");
+        expect(passUndefined).toThrowError("The parameter is null or undefined");
+    });
+});
