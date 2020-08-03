a614f9c022e901eb7f9716bf98c92a1b363fe1b8
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-03 03:06:22 -0700

    :tada: Create and implement devide.js

---
+++ src/1.x.x/scripts/array/math/devide.js
@@ -0,0 +1,19 @@
+import {
+    checkArray
+} from "../../../helpers/check";
+
+export function devide(array, from, to, devider) {
+    if (checkArray(array)) {
+        if (to < array.length) {
+            return array.map(element => ((array.indexOf(element) >= from && array.indexOf(element) < to + 1) && typeof element === "number" && element / devider) || element)
+        } else {
+            throw new Error("The last index of the array is less then 'to' parameter")
+        }
+    }
+}
+
+export function devideAll(array, devider) {
+    if (checkArray(array)) {
+        return array.map(element => (typeof element === "number" && element / devider) || element)
+    }
+}
