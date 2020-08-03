51bc3cfe8a695ea5c2eda033351a3052bec375d0
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-03 05:50:46 -0700

    :wastebasket: Remove error test suite and lift it up to check.spec.js

--- specs/1.x.x/scripts/array/math/abs.spec.js
+++ specs/1.x.x/scripts/array/math/abs.spec.js
@@ -17,39 +17,7 @@ describe("Absolute values for specific range of elements", () => {
         expect(absRange(["a", "b", "c"], 0, 1)).toStrictEqual(["a", "b", "c"]);
     });
 
-    it("should throw an error", () => {
 
-        function passNumber() {
-            absRange(1, 0, 1)
-        }
-
-        function passString() {
-            absRange("array", 0, 1)
-        }
-
-        function passEmptyArray() {
-            absRange([], 0, 1)
-        }
-
-        function passNull() {
-            absRange(null, 0, 1)
-        }
-
-        function passUndefined() {
-            absRange(undefined, 0, 1)
-        }
-
-        function passBigIndex() {
-            const array = [-1, -2, -3];
-            absRange(array, 0, 4); // The last index of array is 2
-        }
-        expect(passNumber).toThrowError("The parameter should be an array");
-        expect(passString).toThrowError("The parameter should be an array");
-        expect(passEmptyArray).toThrowError("The array is empty");
-        expect(passNull).toThrowError("The parameter is null or undefined");
-        expect(passUndefined).toThrowError("The parameter is null or undefined");
-        expect(passBigIndex).toThrowError("The last index of the array is less then 'to' parameter");
-    });
 });
 
 describe("Absolute values for all elements", () => {
@@ -64,34 +32,5 @@ describe("Absolute values for all elements", () => {
     });
     it("should return the same values that are not numbers", () => {
         expect(abs(["a", "b", "c"])).toStrictEqual(["a", "b", "c"]);
-    });
-
-    it("should throw an error", () => {
-
-        function passNumber() {
-            abs(1)
-        }
-
-        function passString() {
-            abs("array")
-        }
-
-        function passEmptyArray() {
-            abs([])
-        }
-
-        function passNull() {
-            abs(null)
-        }
-
-        function passUndefined() {
-            abs(undefined)
-        }
-
-        expect(passNumber).toThrowError("The parameter should be an array");
-        expect(passString).toThrowError("The parameter should be an array");
-        expect(passEmptyArray).toThrowError("The array is empty");
-        expect(passNull).toThrowError("The parameter is null or undefined");
-        expect(passUndefined).toThrowError("The parameter is null or undefined");
     });
 });
