bbf3b3de9e2b002486fe56feb24d8e5b2b6c4b41
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-05 17:35:39 -0700

    :cyclone: Add default value for criteria and provide examples

--- src/1.x.x/scripts/array/order.js
+++ src/1.x.x/scripts/array/order.js
@@ -9,7 +9,7 @@ import {
     checkArray
 } from "../../helpers/check"
 
-export function orderRangeDescBy(array, from, to, criteria) {
+export function orderRangeDescBy(array, from, to, criteria = null) {
     // Order a range of an array by a specific criteria
     // Let's assume that we have this array
     const users = [{
@@ -27,18 +27,22 @@ export function orderRangeDescBy(array, from, to, criteria) {
         }]
         // When the developer call this method like this : orderRangeDescBy(users,0,2,'id')
     const orderedDescUsers = [{
-        id: 3,
-        name: "Eminem"
-    }, {
-        id: 2,
-        name: "Jhon Weak"
-    }, {
-        id: 1,
-        name: "Ala Eddine"
-    }, {
-        id: 4,
-        name: "Karma"
-    }]
+            id: 3,
+            name: "Eminem"
+        }, {
+            id: 2,
+            name: "Jhon Weak"
+        }, {
+            id: 1,
+            name: "Ala Eddine"
+        }, {
+            id: 4,
+            name: "Karma"
+        }]
+        // Other examples :  
+        // orderRangeDescBy([1,2,3,4],0,2) will be [3,2,1,4]
+        // orderRangeDescBy(["a","b","c","d"],0,2) will be ["c","b","a","d"]
+
 
     if (checkArray(array)) {
         // Write your code here and remove the examples above
