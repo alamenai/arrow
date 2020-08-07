3326e9ab358c0db4fcdf52ce39e0e3d469266ac2
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-07 13:52:44 -0700

    :zap: Test omitArray() function

--- specs/1.x.x/scripts/array/omit.spec.js
+++ specs/1.x.x/scripts/array/omit.spec.js
@@ -1,6 +1,7 @@
 import {
     omit,
-    omitMany
+    omitMany,
+    omitArray
 } from "../../../../src/1.x.x/scripts/array/omit"
 
 describe("Remove an element from an array ", () => {
@@ -29,27 +30,27 @@ describe("Remove an element from an array ", () => {
         expect(passUndefinedElement).toThrowError(element + " does not exist");
     });
 });
-describe("Remove multiple elements from an array ", () => {
-    it("should return array without removed elements", () => {
-        expect(omitMany([1, 2, 3, 4, 5, 6], 1, 2, 3)).toStrictEqual([4, 5, 6]);
-        expect(omitMany(["aaa", "baa", "cbb", "dee", "ecc", "faa"], "aaa", "baa")).toStrictEqual(["cbb", "dee", "ecc", "faa"]);
-        expect(omitMany([{
+describe("Remove an element from an array ", () => {
+    it("should return array without removed element", () => {
+        expect(omit([1, 2, 3, 4, 5, 6], 1)).toStrictEqual([2, 3, 4, 5, 6]);
+        expect(omit(["aaa", "baa", "cbb", "dee", "ecc", "faa"], "aaa")).toStrictEqual(["baa", "cbb", "dee", "ecc", "faa"]);
+        expect(omit([{
             a: 1
         }, {
             b: 2
         }, {
             c: 3
-        }], 'a', 'b')).toStrictEqual([{
+        }], 'a')).toStrictEqual([{
+            b: 2
+        }, {
             c: 3
         }]);
-
     });
-    it("should throw an error", () => {
-        const elements = [1, 2, 3]
+});
 
-        function passUndefinedElement() {
-            omit([4, 5], elements)
-        }
-        expect(passUndefinedElement).toThrowError(elements + " does not exist");
+describe("Remove elements from an array based on other array ", () => {
+    it("should return array without removed elements", () => {
+        expect(omitArray([1, 2, 3, 4, 5, 6], [1, 2, 3])).toStrictEqual([4, 5, 6]);
+
     });
 });
