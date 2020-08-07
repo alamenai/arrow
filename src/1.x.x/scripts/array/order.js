823a5692ee848c5ebac5edcd3420a54d2a720fad
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-07 08:55:19 -0700

    Revert ":zap: Implement isObjectArray() and isPrimitive() functions"
    This reverts commit ad4837f21a38d0ab23b34c218e3a1a2323b93b37.

--- src/1.x.x/scripts/array/order.js
+++ src/1.x.x/scripts/array/order.js
@@ -1,37 +1,96 @@
+/**
+ * Author :
+ * Github :
+ * Date :
+ * Unit Test : ./specs/1.x.x/scripts/arrays/order.spec.js
+ */
+
 import {
     checkArray
 } from "../../helpers/check"
 
-import {
-    isPrimitive
-} from "../../helpers/is"
-
 export function orderRangeDescBy(array, from, to, criteria = null) {
+    // Order a range of an array by a specific criteria
+    // criteria is an optional parameter and it's useful only for arrays that contain objects
+    // Let's assume that we have this array
+    const users = [{
+            id: 1,
+            name: "Ala Eddine"
+        }, {
+            id: 2,
+            name: "Jhon Weak"
+        }, {
+            id: 3,
+            name: "Eminem"
+        }, {
+            id: 4,
+            name: "Karma"
+        }]
+        // When the developer call this method like this : orderRangeDescBy(users,0,2,'id')
+    const orderedDescUsers = [{
+            id: 3,
+            name: "Eminem"
+        }, {
+            id: 2,
+            name: "Jhon Weak"
+        }, {
+            id: 1,
+            name: "Ala Eddine"
+        }, {
+            id: 4,
+            name: "Karma"
+        }]
+        // Other examples :  
+        // orderRangeDescBy([1,2,3,4],0,2) will be [3,2,1,4]
+        // orderRangeDescBy(["a","b","c","d"],0,2) will be ["c","b","a","d"]
+
 
     if (checkArray(array)) {
-        if (to < array.length) {
-            if (isPrimitive(array)) {
-                const sortedArray = array.slice(from, to + 1).sort((a, b) => -1)
-                let pointer = 0;
-                for (let index = from; index < to + 1; index++) {
-                    array[index] = sortedArray[pointer]
-                    pointer = pointer + 1;
-                }
-                return array
-            }
-
-        } else {
-            throw new Error("The last index of the array is less then 'to' parameter")
-        }
+        // Write your code here and remove the examples above
+        // Run the test : npm run test:spec
+        // Expected commit: :zap: Implement orderRangeDescBy() function
+        /**
+         * Hint : COMMITS_TEMPLATE to know how to write your commits or see previous commits
+         */
     }
+
 }
 
 export function orderDescBy(array, criteria = null) {
+    // Order an array by a specific criteria
+    // Let's assume that we have this array
+    const users = [{
+            id: 1,
+            name: "Ala Eddine"
+        }, {
+            id: 2,
+            name: "Jhon Weak"
+        }, {
+            id: 3,
+            name: "Eminem"
+        }, {
+            id: 4,
+            name: "Karma"
+        }]
+        // When the developer call this method like this : orderDescBy(users,'id')
+    const orderedDescUsers = [{
+        id: 4,
+        name: "Karma"
+    }, {
+        id: 3,
+        name: "Eminem"
+    }, {
+        id: 2,
+        name: "Jhon Weak"
+    }, {
+        id: 1,
+        name: "Ala Eddine"
+    }]
+
     if (checkArray(array)) {
-        if (isPrimitive(array)) {
-            return array.sort((a, b) => -1)
-        }
-    } else {
-        throw new Error("The last index of the array is less then 'to' parameter")
+        // Write your code here and remove the examples above
+        // Run the test : npm run test:spec
+        // Expected commit : :zap: Implement orderDescBy() function 
     }
+
 }
