2d0c57ab16008bf4ff624922518ebc6a03a8782d
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-07 10:42:41 -0700

    :wastebasket: Delete check.js

--- src/1.x.x/helpers/check.js
+++
@@ -1,6 +0,0 @@
-export function checkArray(array) {
-    if (array === null || array === undefined) throw new Error("The parameter is null or undefined");
-    if (!Array.isArray(array)) throw new Error("The parameter should be an array");
-    if (array.length === 0) throw new Error("The array is empty");
-    return true;
-}
