e07c9c1acab6d7c9a06d2b96a256aa92d9600211
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-07 14:14:46 -0700

    :zap: Implement betweenRange() function

--- src/1.x.x/scripts/array/between.js
+++ src/1.x.x/scripts/array/between.js
@@ -1,7 +1,16 @@
 import {
     isArray,
-} from "../../../helpers/is";
+} from "../../helpers/is";
 
+export function betweenRange(array, from, to, start, end) {
+    if (isArray(array)) {
+        if (to < array.length) {
+            const range = array.slice(from, to + 1)
+            return between(range, start, end)
+        }
+    }
+    return array.every(element => element > start - 1 && element < end + 1)
+}
 export function between(array, start, end) {
     if (isArray(array))
         return array.every(element => element > start - 1 && element < end + 1)
