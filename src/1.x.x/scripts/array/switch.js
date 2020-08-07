dcbcf83b375cbc525f695da2de5ab91e76b6a298
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-07 13:33:16 -0700

    :tada: Create and Implement switch.js

---
+++ src/1.x.x/scripts/array/switch.js
@@ -0,0 +1,25 @@
+import {
+    isArray
+} from "../../helpers/is"
+
+export function switchByIndex(array, switchedIndex, switcherIndex) {
+    if (isArray(array)) {
+        if (!(switchedIndex < array.length && switcherIndex < array.length)) {
+            throw new Error("One or both of the indexes are big then the length of the array")
+        }
+        let switcher = array[switchedIndex];
+        array[switchedIndex] = array[switcherIndex];
+        array[switcherIndex] = switcher;
+        return array;
+    }
+}
+export function switchByValue(array, switchedValue, switcherValue) {
+    if (isArray(array)) {
+        const index = array.indexOf(switchedValue);
+        const switcherIndex = array.indexOf(switcherValue);
+        if (index === -1 || switcherIndex === -1) {
+            throw new Error("One or both of the values do not exist in the array")
+        }
+        return switchByIndex(array, index, switcherIndex)
+    }
+}
