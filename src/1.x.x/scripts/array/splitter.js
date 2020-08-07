57b5441fec927f6d62aae32f278e4bd2a5ab73ba
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-07 13:36:41 -0700

    :tada: Create and prototype splitter.js

---
+++ src/1.x.x/scripts/array/splitter.js
@@ -0,0 +1,9 @@
+export default function splitIntoArrays(array, ) {
+    // Split an array into two separated arrays
+    // [1, null, 3, 'item'] will be [1,null] and [3,'item']
+}
+
+export default function splitIntoObjects(array) {
+    // Convert items to Objects with indexed keys
+    // [1, null, 3, 'item'] will be { 1: 1 , 2: null} , { 3: 3 ,  4: null }
+}
