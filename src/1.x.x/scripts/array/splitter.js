a144ea1af9fadcb4f8680f7ce56e4d412b842d34
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-08 17:57:17 -0700

    :wastebasket: Remove splitter.js module

--- src/1.x.x/scripts/array/splitter.js
+++
@@ -1,9 +0,0 @@
-export default function splitIntoArrays(array, ) {
-    // Split an array into two separated arrays
-    // [1, null, 3, 'item'] will be [1,null] and [3,'item']
-}
-
-export default function splitIntoObjects(array) {
-    // Convert items to Objects with indexed keys
-    // [1, null, 3, 'item'] will be { 1: 1 , 2: null} , { 3: 3 ,  4: null }
-}
