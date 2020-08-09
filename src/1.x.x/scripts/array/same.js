fef8c11dcc0d7439aae4df8ccac1a5dba55a56f0
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-08 17:54:21 -0700

    :beetle: Fix array type checking by adding isArray() from is module

--- src/1.x.x/scripts/array/same.js
+++ src/1.x.x/scripts/array/same.js
@@ -4,7 +4,7 @@ export function same(...arrays) {
     const args = Array.from([...arrays])
     let initArray = args[0];
     let sameElements = new Set();
-    args.forEach(array => args.indexOf(array) !== 0 &&
+    args.forEach(array => isArray(array) && args.indexOf(array) !== 0 &&
         array.forEach(value => initArray.includes(value) && sameElements.add(value)))
     return Array.from(sameElements)
 }
