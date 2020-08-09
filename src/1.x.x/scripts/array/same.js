29005eca106368e00cae41cf5b4090200b18680a
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-08 17:09:36 -0700

    :cyclone: Rename firstArray to initArray

--- src/1.x.x/scripts/array/same.js
+++ src/1.x.x/scripts/array/same.js
@@ -2,9 +2,9 @@ import { isArray } from "../../helpers/is";
 export function same(...arrays) {
     if (arrays.length === 1) return arrays[0]
     const args = Array.from([...arrays])
-    let firstArray = args[0];
+    let initArray = args[0];
     let sameElements = new Set();
     args.forEach(array => args.indexOf(array) !== 0 &&
-        array.forEach(value => firstArray.includes(value) && sameElements.add(value)))
+        array.forEach(value => initArray.includes(value) && sameElements.add(value)))
     return Array.from(sameElements)
 }
