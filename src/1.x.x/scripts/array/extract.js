3f2d12083bc75db15d202bb57aa5ac4ef2d27f94
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-08 17:47:46 -0700

    :tada: Prototype extract.js

---
+++ src/1.x.x/scripts/array/extract.js
@@ -0,0 +1,19 @@
+export function extractNull(array) {}
+
+export function extractSymbol(array) {}
+
+export function extractString(array) {}
+
+export function extractNumber(array) {}
+
+export function extractObject(array) {}
+
+export function extractArray(array) {}
+
+export function extractOdd(array) {}
+
+export function extractPair(array) {}
+
+export function extractByOddIndex(array) {}
+
+export function extractByPaiIndex(array) {}
