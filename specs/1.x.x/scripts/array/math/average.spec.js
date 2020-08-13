b620f8daa23e6719d3b6772b5127556f2861be33
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-13 04:24:53 -0700

    :tada: Create average test suite for Testing average.js

---
+++ specs/1.x.x/scripts/array/math/average.spec.js
@@ -0,0 +1,41 @@
+import average from "../../../../../src/1.x.x/scripts/array/math/average"
+
+// Make sure to open an issue before you create pull request for additional test
+
+// Make sure to open an issue if you find an bug ( Not grammar error ) in this test
+
+describe("Calculate the average of an array", () => {
+    it("should return  positive value for positive elements", () => {
+        expect(average([1, 2, 3])).toStrictEqual((1 + 2 + 3) / 3);
+    });
+    it("should return 0 when an element is equal to  Infinity", () => {
+        expect(average([1, 2, Infinity])).toStrictEqual((1 + 2 + Infinity) / 3);
+    });
+    it("should return 0 when all elements are equal to Infinity", () => {
+        expect(average([Infinity, Infinity, Infinity])).toStrictEqual((Infinity + Infinity + Infinity) / 3);
+    });
+    it("should return negative value for negative elements", () => {
+        expect(average([-1, -2, -3])).toStrictEqual((-1(-2) + (-2)) / 3);
+    });
+    it("should make cohersion for numbers-like strings", () => {
+        expect(average(["1", "2", "3"])).toStrictEqual((1 + 2 + 3) / 3);
+    });
+    it("should return NaN value for NaN elements", () => {
+        expect(average([NaN, NaN, NaN])).toStrictEqual((NaN + NaN + NaN) / 3);
+    });
+
+    it("should throw an error when the elements have type of string or undefined", () => {
+
+        function hasStringType() {
+            const arr = ["a", "b", "c"]
+            average(arr)
+        }
+        function hasUndefined() {
+            const arr = [undefined, undefined, undefined]
+            average(arr)
+        }
+        expect(hasStringType).toThrowError("The elements of an array should have a Number type");
+        expect(hasUndefined).toThrowError("The elements of an array should have a Number type");
+    });
+
+});
