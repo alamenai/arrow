8ff05971f7019d17d150b087215599b32a46e4f0
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-23 16:11:27 -0700

    :beetle: Fix some() by checking elements with string type

--- src/1.x.x/helpers/has.js
+++ src/1.x.x/helpers/has.js
@@ -1,16 +1,19 @@
-import {
-    isObjectArray
-} from "../../1.x.x/helpers/is"
+import { isObjectArray, isStringArray } from "../../1.x.x/helpers/is"
 
 export function hasElement(array, element) {
     return isObjectArray(array) ? hasObject(array, element) : hasValue(array, element)
 }
 
 function hasValue(array, value) {
-
+    if (isStringArray(array)) {
+        return array.some(element => element.toLowerCase() === value.toLowerCase())
+    }
     return array.some(element => element === value)
 }
 
 function hasObject(array, key) {
+    if (isStringArray(array)) {
+        return array.some(element => Object.keys(element)[0].toLowerCase() === key.toLowerCase())
+    }
     return array.some(element => Object.keys(element)[0] === key)
 }
