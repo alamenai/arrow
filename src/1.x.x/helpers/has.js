0ddd3d8dadcc4556ff72b29737592d5444854505
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-23 16:18:54 -0700

    :cyclone: Replace normal functions with arrow functions style

--- src/1.x.x/helpers/has.js
+++ src/1.x.x/helpers/has.js
@@ -1,17 +1,17 @@
 import { isObjectArray, isStringArray } from "../../1.x.x/helpers/is"
 
-export function hasElement(array, element) {
+export const hasElement = (array, element) => {
     return isObjectArray(array) ? hasObject(array, element) : hasValue(array, element)
 }
 
-function hasValue(array, value) {
+const hasValue = (array, value) => {
     if (isStringArray(array)) {
         return array.some(element => element.toLowerCase() === value.toLowerCase())
     }
     return array.some(element => element === value)
 }
 
-function hasObject(array, key) {
+const hasObject = (array, key) => {
     if (isStringArray(array)) {
         return array.some(element => Object.keys(element)[0].toLowerCase() === key.toLowerCase())
     }
--- src/1.x.x/helpers/is.js
+++ src/1.x.x/helpers/is.js
@@ -1,24 +1,24 @@
-export function isArray(array) {
+export const isArray = array => {
     if (array === null || array === undefined) throw new Error("The parameter is null or undefined");
     if (!Array.isArray(array)) throw new Error("The parameter should be an array");
     if (array.length === 0) throw new Error("The array is empty");
     return true;
 }
-export function isNumberArray(array) {
+export const isNumberArray = array => {
     return array.every(element => typeof element === "number")
 }
 
-export function isStringArray(array) {
+export const isStringArray = array => {
     return array.every(element => typeof element === "string")
 }
 
-export function isBooleanArray(array) {
+export const isBooleanArray = (array) => {
     return array.every(element => typeof element === "boolean")
 }
-export function isObjectArray(array) {
+export const isObjectArray = array => {
     return array.every(element => typeof element === "object")
 }
-export function isPrimitiveArray(array) {
+export const isPrimitiveArray = array => {
     if (array.every(element => typeof element === "object") ||
         array.every(element => typeof element === "boolean") ||
         array.every(element => typeof element === "number") ||
