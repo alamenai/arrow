1a6468fbe65ba027ee8a9f1a2b82d0d83d4c9d8e
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-24 10:32:00 -0700

    :cyclone: Replace normal functions with arrow functions

--- src/1.x.x/scripts/array/math/abs.js
+++ src/1.x.x/scripts/array/math/abs.js
@@ -1,8 +1,6 @@
-import {
-    isArray
-} from "../../../helpers/is";
+import { isArray } from "../../../helpers/is";
 
-export function absRange(array, from, to) {
+export const absRange = (array, from, to) => {
     if (isArray(array)) {
         if (to < array.length) {
             return array.map(element => ((array.indexOf(element) >= from && array.indexOf(element) < to + 1) && typeof element === "number" && Math.abs(element)) || element)
@@ -12,7 +10,7 @@ export function absRange(array, from, to) {
     }
 }
 
-export function abs(array) {
+export const abs = array => {
     if (isArray(array)) {
         return array.map(element => (typeof element === "number" && Math.abs(element)) || element)
     }
