5a1ae31423dac9b886b070d9a297bb453aed2e00
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-25 11:21:14 -0700

    :tada: Create and implement index.js for exporting modules

---
+++ src/1.x.x/scripts/array/has/index.js
@@ -0,0 +1,142 @@
+import { equalAll } from "./equal"
+import { uniqueAll } from "./unique"
+
+import {
+    hasAnyArray,
+    hasAtLeastArray,
+    hasExactArray,
+    hasOnceArray,
+    hasOnlyArray,
+    hasPairArray
+} from "./array"
+
+import {
+    hasAnyBoolean,
+    hasAtLeastBoolean,
+    hasExactBoolean,
+    hasOnceBoolean,
+    hasOnlyBoolean,
+    hasPairBoolean
+} from "./boolean"
+
+import {
+    hasAnyInfinity,
+    hasAtLeastInfinity,
+    hasExactInfinity,
+    hasOnceInfinity,
+    hasOnlyInfinity,
+    hasPairInfinity
+} from "./infinity"
+
+import {
+    hasAnyNaN,
+    hasAtLeastNaN,
+    hasExactNaN,
+    hasOnceNaN,
+    hasOnlyNaN,
+    hasPairNaN
+} from "./nan"
+
+import {
+    hasAnyNull,
+    hasAtLeastNull,
+    hasExactNull,
+    hasOnceNull,
+    hasOnlyNull,
+    hasPairNull
+} from "./null"
+
+import {
+    hasAnyNumber,
+    hasAtLeastNumber,
+    hasExactNumber,
+    hasOnceNumber,
+    hasOnlyNumber,
+    hasPairNumber
+} from "./number"
+
+import {
+    hasAnyObject,
+    hasAtLeastObject,
+    hasExactObject,
+    hasOnceObject,
+    hasOnlyObject,
+    hasPairObject
+} from "./object"
+
+import {
+    hasAnyString,
+    hasAtLeastString,
+    hasExactString,
+    hasOnceString,
+    hasOnlyString,
+    hasPairString
+} from "./string"
+
+import {
+    hasAnySymbol,
+    hasAtLeastSymbol,
+    hasExactSymbol,
+    hasOnceSymbol,
+    hasOnlySymbol,
+    hasPairSymbol
+} from "./symbol"
+
+export {
+    equalAll,
+    uniqueAll,
+    hasAnyArray,
+    hasAtLeastArray,
+    hasExactArray,
+    hasOnceArray,
+    hasOnlyArray,
+    hasPairArray,
+    hasAnyBoolean,
+    hasAtLeastBoolean,
+    hasExactBoolean,
+    hasOnceBoolean,
+    hasOnlyBoolean,
+    hasPairBoolean,
+    hasAnyInfinity,
+    hasAtLeastInfinity,
+    hasExactInfinity,
+    hasOnceInfinity,
+    hasOnlyInfinity,
+    hasPairInfinity,
+    hasAnyNaN,
+    hasAtLeastNaN,
+    hasExactNaN,
+    hasOnceNaN,
+    hasOnlyNaN,
+    hasPairNaN,
+    hasAnyNull,
+    hasAtLeastNull,
+    hasExactNull,
+    hasOnceNull,
+    hasOnlyNull,
+    hasPairNull,
+    hasAnyNumber,
+    hasAtLeastNumber,
+    hasExactNumber,
+    hasOnceNumber,
+    hasOnlyNumber,
+    hasPairNumber,
+    hasAnyObject,
+    hasAtLeastObject,
+    hasExactObject,
+    hasOnceObject,
+    hasOnlyObject,
+    hasPairObject,
+    hasAnyString,
+    hasAtLeastString,
+    hasExactString,
+    hasOnceString,
+    hasOnlyString,
+    hasPairString,
+    hasAnySymbol,
+    hasAtLeastSymbol,
+    hasExactSymbol,
+    hasOnceSymbol,
+    hasOnlySymbol,
+    hasPairSymbol
+}
