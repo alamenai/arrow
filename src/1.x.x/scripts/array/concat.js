a37b6b5e05a31887d4eb027235bf338f1028fd10
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-25 09:50:32 -0700

    :tada: Implement concat function

--- src/1.x.x/scripts/array/concat.js
+++ src/1.x.x/scripts/array/concat.js
@@ -1,31 +1,23 @@
-/**
- * Author :
- * Github :
- * Date :
- * Unit Test : ./specs/concat.spec.js
- */
+import { isArray } from "../../helpers/is"
 
-export function concatRange(array, from, to, value) {
-    // Concat the value with a portion of items
-    const from = 1;
-    const to = 3;
-    const value = "github";
-    const array = [1, 2, 3, 4, 5, 6]
-        // When the developer call the API like this : concatRange(array,from,to,value)
-    const concatedArray = [1, "2github", "3github", "4github", 5, 6]
-    const concatedArray2 = [1, 25, 35, 45, 5, 6] // supposed value = 5
-    if (isArray(array)) {
-        // Write your code here and remove the examples above
-        // Run the test : npm run test:spec
-        // Expected commit: :zap: Implement concatRange() function
+export const concat = (array, element) => {
+    if (!element) {
+        throw 'null or undefined values are not accepted for the concatenation'
     }
-
-}
-export function concat(array, value) {
-    // Concat the value with all items
     if (isArray(array)) {
-        // Write your code here and remove the examples above
-        // Run the test : npm run test:spec
-        // Expected commit: :zap: Implement concat() function
+        return array.map(value => {
+            if (!value) {
+                return value
+            }
+            if ((typeof value === "number" || typeof value === "string") && (typeof element === "number")) {
+                return Number.parseFloat(String(value).concat(String(element)))
+            }
+            if (typeof value === "string" && typeof element === "string") {
+                return value.concat(element)
+            }
+            if (typeof value === "number" && typeof element === "string") {
+                return String(value).concat(element)
+            }
+        })
     }
 }
