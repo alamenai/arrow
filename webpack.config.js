d4229f677d8362c5fb9c93956ed36f9194e1b83b
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-24 19:03:19 -0700

    :tada: Create webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+
+module.exports = {
+    mode: "development",
+    entry: {
+        arrow: "./src/1.x.x/index",
+    },
+    plugins: [
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './build',
+    },
+    output: {
+        filename: "[name]-alpha.js",
+        path: path.resolve(__dirname, 'build'),
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+}
